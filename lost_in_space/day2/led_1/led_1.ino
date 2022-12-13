0b199800ed66f9c8c185f6e5d9149bab861dc496
dcsimpson01 <dcsimpson01@gmail.com>
2022-12-12 21:59:59 -0600

    updates to led_1.ino file

--- lost_in_space/day2/led_1/led_1.ino
+++ lost_in_space/day2/led_1/led_1.ino
@@ -1,16 +1,21 @@
-int Light = 12; // Hero board pin 12
+int Light = 11; // Hero board pin 13
+int Light2 = 13;
 
 void setup() {
   // initialize a digital pin as an output, then set its value to High (5 volts)
   pinMode(Light, OUTPUT);  // assigns output function to pin number 12
   //digitalWrite(Light, LOW)  //Set its voltage to low
   digitalWrite(Light, LOW); // Turn it on
+  pinMode(Light2, OUTPUT);
+  digitalWrite(Light2, HIGH);
 }
 
 void loop() {
   // main code to run repeatedly:
 digitalWrite(Light, LOW); // 0 Volts
+digitalWrite(Light2, HIGH); // 0 Volts
 delay(2000); 
 digitalWrite(Light, HIGH); // 5 Volts
+digitalWrite(Light2, LOW); // 5 Volts
 delay(100); // 500 miliseconds
 }
